9f29367da061bfdfc397492a08d289c231aaf03e
Rumlor <mehmet97dogan@gmail.com>
2022-11-30 07:24:00 +0300

    auth changes for ui

---
+++ src/appcomponents/user/UserInfo.js
@@ -0,0 +1,21 @@
+class  UserInfo extends React.Component {
+
+
+    state = {
+        //define state
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
