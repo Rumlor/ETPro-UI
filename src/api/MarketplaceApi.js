8381e0820935206e76518dc0f454b74328ebd0e7
= <=>
2022-12-22 21:57:03 +0300

    added api changes

--- src/api/MarketplaceApi.js
+++ src/api/MarketplaceApi.js
@@ -2,14 +2,11 @@ import {API,globalApiWrapper} from "./ApiList.js"
 
 import {getHttpHeaderWithToken,prepareRequestOptions} from "../services/HttpHeaderAndMiscService";
 
+   const {addMarketPlace,getMarketPlaceList,deleteMarketPlace,updateMarketPlace} = {...globalApiWrapper.marketPlaceApi};
+
    export  const POST_MARKETPLACE = (body,onSuccess,onFail)=>{
-
-       const reqOptions  = prepareRequestOptions(
-                    globalApiWrapper.marketPlaceApi.addMarketPlace.httpMethod,
-                    getHttpHeaderWithToken(),
-                    body)
-
-         fetch(globalApiWrapper.marketPlaceApi.addMarketPlace.url,reqOptions)
+       const reqOptions  = prepareRequestOptions(addMarketPlace.httpMethod,getHttpHeaderWithToken(),body)
+         fetch(addMarketPlace.url,reqOptions)
              .then(response=>response.json())
              .then((response)=> {
                  if (response.result){
@@ -20,19 +17,10 @@ import {getHttpHeaderWithToken,prepareRequestOptions} from "../services/HttpHead
                  }
              })
              .catch(reason => onFail(reason))
-
-
-
     }
    export const GET_MARKETPLACES = (body,OnSuccess,OnFail) => {
-
-        const reqOptions  = prepareRequestOptions(
-            globalApiWrapper.marketPlaceApi.getMarketPlaceList.httpMethod,
-            getHttpHeaderWithToken(),
-            body)
-        console.log('header for marketplace get')
-        console.log(reqOptions)    
-        fetch(globalApiWrapper.marketPlaceApi.getMarketPlaceList.url,reqOptions)
+        const reqOptions  = prepareRequestOptions(getMarketPlaceList.httpMethod,getHttpHeaderWithToken(),body)
+        fetch(getMarketPlaceList.url,reqOptions)
             .then(response=>response.json())
             .then((response)=> {
                 if (response.result){
@@ -42,13 +30,9 @@ import {getHttpHeaderWithToken,prepareRequestOptions} from "../services/HttpHead
                 }
             })
             .catch(reason => OnFail(reason))
-
     }
    export const DELETE_MARKETPLACE = (pathVariable,onSuccess,onFail)=>{
-       const deleteMarketPlace = globalApiWrapper.marketPlaceApi.deleteMarketPlace; 
-       const reqOptions =  prepareRequestOptions(
-                                deleteMarketPlace.httpMethod,
-                                getHttpHeaderWithToken(),null);
+       const reqOptions =  prepareRequestOptions(deleteMarketPlace.httpMethod,getHttpHeaderWithToken(),null);
        fetch(deleteMarketPlace.url.concat("/").concat(pathVariable),reqOptions)
            .then(response=>response.json())
            .then((response)=> {
@@ -61,10 +45,7 @@ import {getHttpHeaderWithToken,prepareRequestOptions} from "../services/HttpHead
            .catch(reason => onFail(reason))
    }
    export const PUT_MARKETPLACE = (body,onSuccess,onFail)=>{
-       const updateMarketPlace = globalApiWrapper.marketPlaceApi.updateMarketPlace; 
-       const reqOptions =  prepareRequestOptions(
-                        updateMarketPlace.httpMethod,
-                        getHttpHeaderWithToken(),body);
+       const reqOptions =  prepareRequestOptions(updateMarketPlace.httpMethod,getHttpHeaderWithToken(),body);
        fetch(updateMarketPlace.url,reqOptions)
            .then(response=>response.json())
            .then((response)=> {
