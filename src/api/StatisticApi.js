f8a61c32e1d0e0645ec793733375e88d201151b2
= <=>
2022-12-23 08:52:39 +0300

    api changes

--- src/api/StatisticApi.js
+++ src/api/StatisticApi.js
@@ -1,8 +1,10 @@
 import {API, globalApiWrapper} from "./ApiList.js"
 import {getHttpHeaderWithToken,prepareRequestOptions} from "../services/HttpHeaderAndMiscService";
 
+const {getStatistics} = globalApiWrapper.statisticApi
+
+
 export const GET_STATISTICS = (onSuccessComponent,onFailComponent)=>{
-    const getStatistics = globalApiWrapper.statisticApi.getDashboardStatistics;
     const reqOptions = prepareRequestOptions(getStatistics.httpMethod,getHttpHeaderWithToken(),null);
     fetch(getStatistics.url,reqOptions)
     .then(response=>response.json())
